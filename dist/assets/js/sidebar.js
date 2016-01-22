528c9ca1821bc51e0540db2a730f04b8270a42db
Cathy Dutton <info@cathydutton.co.uk>
2016-01-22 19:18:55 +0000

    Add sidebar amendments

--- dist/assets/js/sidebar.js
+++ dist/assets/js/sidebar.js
@@ -8,17 +8,17 @@ window.onload = function() {
 	// SIDEBAR
 	var toggleNav = document.getElementById('js-header__icon');
 	var sidebar = document.querySelector('.sidebar');
-	var wrapper = document.getElementById('wrapper');
+	// var wrapper = document.getElementById('wrapper');
 	var sidebarLink = document.querySelectorAll('.sidebar__anchor, .find-me__anchor'), i;
-	var width = window.innerWidth;
+	//var width = window.innerWidth;
 
 
 	// set menu to open on desktop
-	if (width > 1020) {
-		sidebar.className = sidebar.className.replace( /(?:^|\s)sidebar--closed(?!\S)/g , ' sidebar--open' );
-		wrapper.className = wrapper.className.replace( /(?:^|\s)wrapper--deactive(?!\S)/g , ' wrapper--active' );
-		toggleNav.className = toggleNav.className.replace( /(?:^|\s)button--menu--closed(?!\S)/g , ' button--menu--open' );
-	}
+	// if (width > 1020) {
+	// 	sidebar.className = sidebar.className.replace( /(?:^|\s)sidebar--closed(?!\S)/g , ' sidebar--open' );
+	// 	wrapper.className = wrapper.className.replace( /(?:^|\s)wrapper--deactive(?!\S)/g , ' wrapper--active' );
+	// 	toggleNav.className = toggleNav.className.replace( /(?:^|\s)button--menu--closed(?!\S)/g , ' button--menu--open' );
+	// }
 
 	// set focus based on menu class
 	if (sidebar.className.match(/(?:^|\s)sidebar--closed(?!\S)/) ) {
@@ -42,7 +42,7 @@ window.onload = function() {
 	function changeClass() {
 		if ( sidebar.className.match(/(?:^|\s)sidebar--closed(?!\S)/) ) {
 		      sidebar.className = sidebar.className.replace( /(?:^|\s)sidebar--closed(?!\S)/g , ' sidebar--open' );
-					wrapper.className = wrapper.className.replace( /(?:^|\s)wrapper--active(?!\S)/g , ' wrapper--deactive' );
+					// wrapper.className = wrapper.className.replace( /(?:^|\s)wrapper--active(?!\S)/g , ' wrapper--deactive' );
 					toggleNav.className = toggleNav.className.replace( /(?:^|\s)button--menu--closed(?!\S)/g , ' button--menu--open' );
 		      // Allow focus of Sidebar Elements
 					for (i = 0; i < sidebarLink		.length; ++i) {
@@ -52,7 +52,7 @@ window.onload = function() {
 		  }
 		  else if ( sidebar.className.match(/(?:^|\s)sidebar--open(?!\S)/) ) {
 		      sidebar.className = sidebar.className.replace( /(?:^|\s)sidebar--open(?!\S)/g , ' sidebar--closed' );
-					wrapper.className = wrapper.className.replace( /(?:^|\s)wrapper--deactive(?!\S)/g , ' wrapper--active' );
+					// wrapper.className = wrapper.className.replace( /(?:^|\s)wrapper--deactive(?!\S)/g , ' wrapper--active' );
 					toggleNav.className = toggleNav.className.replace( /(?:^|\s)button--menu--open(?!\S)/g , ' button--menu--closed' );
 					// Remove focus of Sidebar Elements
 					for (i = 0; i < sidebarLink.length; ++i) {
