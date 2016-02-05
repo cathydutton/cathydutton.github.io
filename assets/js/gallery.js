95893aa75b3aee634d4a35c750d4ef6a0d538fbe
Cathy Dutton <info@cathydutton.co.uk>
2016-02-05 21:23:44 +0000

    Add all assets for portfolio page

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -7,14 +7,15 @@ window.onload = function() {
 
 	// GALLERY
   var close = document.getElementById('close'),
-  pop = document.getElementById('pop-up'),
-  popTitle = document.getElementById('pop-title'),
-  popText = document.getElementById('pop-text'),
-  popImage = document.getElementById('pop-image-img'),
+  pop = document.getElementById('gallery-pop-up'),
+  popTitle = document.getElementById('gallery-title'),
+  popText = document.getElementById('gallery-text'),
+  popImage = document.getElementById('gallery-image'),
   elements = document.querySelectorAll(".image-wrapper__image"),
-  next = document.getElementById('next'),
-  prev = document.getElementById('prev'),
-  wrapper = document.querySelector('.wrapper');
+  wrapper = document.querySelector('.wrapper'),
+  next = document.getElementById('gallery-next'),
+  prev = document.getElementById('gallery-prev');
+
 
 
 	var images = [].slice.call(elements);
@@ -47,10 +48,7 @@ window.onload = function() {
     }
 
     close.onclick = function() {
-      pop.style.display = "none";
-    }
-
-		wrapper.onclick = function() {
+      wrapper.className = wrapper.className.replace( /(?:^|\s)lightbox--open(?!\S)/g , ' lightbox--closed' );
       pop.style.display = "none";
     }
 
@@ -58,13 +56,15 @@ window.onload = function() {
 
   var displayStuff = function(myNum) {
 
+    wrapper.className = wrapper.className.replace( /(?:^|\s)lightbox--closed(?!\S)/g , ' lightbox--open' );
+
     pop.style.display = "block";
     pop.focus();
     pop.setAttribute('tabindex', '-1');
 
     alt = images[myNum].getAttribute('alt');
     src = images[myNum].getAttribute('src');
-    newSrc = src.replace("thumbs", "full");
+    newSrc = src.replace("thumbs", "portfolio");
     dataTitle = images[myNum].getAttribute('data-title');
     dataText = images[myNum].getAttribute('data-text');
 
